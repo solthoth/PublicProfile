aab11fc72a0d71e17541af690c9160b77e9d0b78
Carlos C Barajas Jr <soldotsol@gmail.com>
2017-12-10 16:21:33 -0800

    Initial site design

--- src/SolThoth.PublicProfile.Website/Program.cs
+++ src/SolThoth.PublicProfile.Website/Program.cs
@@ -19,7 +19,7 @@ namespace SolThoth.PublicProfile.Website
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+                .UseStartup<Startup>().UseContentRoot(Directory.GetCurrentDirectory())
                 .Build();
     }
 }
